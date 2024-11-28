65744d64dad7b83a4250eb60c91335c3514918f7
club-courier <club.courier.international@gmail.com>
2024-11-28 23:00:47 +0530

    view

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 const hbs = require("hbs");
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 
