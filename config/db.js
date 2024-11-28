28b2df894bd04365974255b35b8024a85ff0007b
club-courier <club.courier.international@gmail.com>
2024-11-28 23:18:36 +0530

    66

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+console.log("DB File Loaded Start");
 mongoose
   .connect(
     "mongodb+srv://clubcourierinternational:Smit123@clubdemo.nrzzh.mongodb.net/?retryWrites=true&w=majority&appName=ClubDemo"
@@ -10,3 +10,4 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err.message);
   });
+console.log("DB File Loaded Ending");
